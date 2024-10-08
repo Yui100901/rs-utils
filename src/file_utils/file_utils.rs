ef99bdac4a0b2735f6ae5a110a0279e97ebe26b6
Yui100901 <973800960@qq.com>
2024-10-08 13:45:26 +0800

    文件目录遍历返回绝对路径

--- src/file_utils/file_utils.rs
+++ src/file_utils/file_utils.rs
@@ -1,6 +1,6 @@
 use std::fs;
 use std::io;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
 /// 遍历给定目录并返回文件路径列表
 /// `recursive` 参数表明是否递归遍历子目录
@@ -8,11 +8,13 @@ pub fn traverse_dir_files(dir: &str, recursive: bool) -> io::Result<(Vec<PathBuf
     let mut files = Vec::new();
     let mut dirs = Vec::new();
 
+    let dir_path = Path::new(dir).canonicalize()?;
+
     if recursive {
         // 递归遍历目录
-        for entry in fs::read_dir(dir)? {
+        for entry in fs::read_dir(&dir_path)? {
             let entry = entry?;
-            let path = entry.path();
+            let path = entry.path().canonicalize()?;
             if path.is_dir() {
                 dirs.push(path.clone());
                 let (sub_dirs, sub_files) = traverse_dir_files(path.to_str().unwrap(), true)?;
@@ -24,9 +26,9 @@ pub fn traverse_dir_files(dir: &str, recursive: bool) -> io::Result<(Vec<PathBuf
         }
     } else {
         // 非递归遍历目录
-        for entry in fs::read_dir(dir)? {
+        for entry in fs::read_dir(&dir_path)? {
             let entry = entry?;
-            let path = entry.path();
+            let path = entry.path().canonicalize()?;
             if path.is_dir() {
                 dirs.push(path);
             } else {
