258a2446e21202ab5e331627bebedc43d206933d
Yui100901 <973800960@qq.com>
2024-10-27 20:10:26 +0800

    常用git命令

--- src/git_utils/mod.rs
+++ src/git_utils/mod.rs
@@ -1,29 +1,38 @@
 use crate::command_utils;
+use std::env::args;
 use std::io::{self};
 
 pub fn clone_default(url: &str, branch: &str, dir: &str) -> Result<String, io::Error> {
-    command_utils::run_command("git", &["clone", "--branch", branch, url, dir])
+    let args = &["clone", "--branch", branch, url, dir];
+    command_utils::run_command("git", args)
 }
 
 pub fn clone_single_branch(url: &str, branch: &str, dir: &str) -> Result<String, io::Error> {
-    command_utils::run_command(
-        "git",
-        &["clone", "--single-branch", "--branch", branch, url, dir],
-    )
+    let args = &["clone", "--single-branch", "--branch", branch, url, dir];
+    command_utils::run_command("git", args)
 }
 
 pub fn clone_latest(url: &str, branch: &str, dir: &str) -> Result<String, io::Error> {
+    let args = &[
+        "clone",
+        "--single-branch",
+        "--branch",
+        branch,
+        "--depth",
+        "1",
+        url,
+        dir,
+    ];
     command_utils::run_command(
         "git",
-        &[
-            "clone",
-            "--single-branch",
-            "--branch",
-            branch,
-            "--depth",
-            "1",
-            url,
-            dir,
-        ],
+        args
+    )
+}
+
+pub fn pull()-> Result<String, io::Error> {
+    let args = &["pull"];
+    command_utils::run_command(
+        "git",
+        args
     )
 }
