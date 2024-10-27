14e80e1c1d73210fea0323fdf062a0fa0ad2f2ad
Yui100901 <973800960@qq.com>
2024-10-27 20:11:08 +0800

    日志输出

--- src/log_utils/mod.rs
+++ src/log_utils/mod.rs
@@ -20,13 +20,13 @@ pub fn init_logger() {
                 };
                 writeln!(
                     buf,
-                    "{}{:<5} - {}:{} - {}{}",
+                    "{}{:<5}{} - {}:{} - {}",
                     color,
                     record.level(),
+                    COLOR_RESET,
                     record.file().unwrap_or("unknown"),
                     record.line().unwrap_or(0),
                     record.args(),
-                    COLOR_RESET
                 )
             })
             .filter(None, LevelFilter::Info)
